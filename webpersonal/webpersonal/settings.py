dee97ae62ce02f235fc86b6f673beb61be3012f4
gino <padovanigino15@hotmail.com>
2024-05-23 18:36:36 -0300

    Update settings.py

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -21,10 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v#1lcxai$ek-w=61=jy=fbl=&=+h#+8gh581ylu7#9n&d8opj*'
+# SECRET_KEY = 'django-insecure-v#1lcxai$ek-w=61=jy=fbl=&=+h#+8gh581ylu7#9n&d8opj*'
+
+# Leer la clave secreta de un archivo
+with open('/apppractica/webpersonalkey.txt') as f:
+    SECRET_KEY = f.read().strip() 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
 ALLOWED_HOSTS = []
 
